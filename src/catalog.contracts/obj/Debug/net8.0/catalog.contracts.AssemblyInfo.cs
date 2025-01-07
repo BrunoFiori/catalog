ad7b2b3f51fb939c5a09be42db8735990269825d
Bruno Canteiro Fiori <brunocfiori@hotmail.com>
2025-01-07 18:32:55 -0300

    Start to create storages logic, that will contains storage items

--- src/catalog.contracts/obj/Debug/net8.0/catalog.contracts.AssemblyInfo.cs
+++ src/catalog.contracts/obj/Debug/net8.0/catalog.contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("catalog.contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1+086db5b62c7c89f7c7ff0373d81e47233ec54026")]
 [assembly: System.Reflection.AssemblyProductAttribute("catalog.contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("catalog.contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
